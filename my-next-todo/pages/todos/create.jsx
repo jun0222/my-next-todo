8099a616f1aa8debbd3d5dc4bac83752d9e0efcc
jun0222 <nextseason6@gmail.com>
2021-10-09 00:20:27 +0900

    Added temporary todo creation function to create.jsx.

--- my-next-todo/pages/todos/create.jsx
+++ my-next-todo/pages/todos/create.jsx
@@ -1,8 +1,64 @@
+import { Grid } from '@material-ui/core'
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import { useState } from 'react';
+
+/* ・TODOタイトル候補<br />
+・TODO作成 */
+
 export default function Home() {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState({});
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const addTaskToTodos = () => {
+        setTodo(todo.title = title);
+        setTodo(todo.content = content);
+
+        const newTodos = [...todos, todo];
+        setTodos(newTodos);
+
+        setTodo({});
+        setTitle('');
+        setContent('');
+    }
+
+    const getInputTextForTitle = (event) => setTitle(event.target.value);
+    const getInputTextForContent = (event) => setContent(event.target.value);
+
     return (
-        <div>
-            ・TODOタイトル候補<br />
-            ・TODO作成
-        </div>
+        <>
+            <Grid container>
+                <Grid sm={2}/>
+                <Grid lg={8} sm={8} spacing={10}>
+                <TextField
+                    placeholder="タイトル"
+                    margin="normal"
+                    value={title}
+                    onChange={getInputTextForTitle}
+                />
+                <TextField
+                    placeholder="内容"
+                    margin="normal"
+                    fullWidth
+                    value={content}
+                    onChange={getInputTextForContent}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    margin="normal"
+                    onClick={addTaskToTodos}
+                >追加</Button>
+                {todos.map((todo, index)=>{
+                    return (
+                        <p>{todo.title}:{todo.content}</p>
+                    )
+                })}
+                </Grid>
+            </Grid>
+        </>
     )
 }
