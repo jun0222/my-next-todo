61cb184eb58c6466958c777cecac54399db4f721
JunHatanaka <nextseason6@gmail.com>
2021-10-14 22:45:46 +0900

    The list function of todos was completed once.

--- my-next-todo/src/pages/todos.jsx
+++ my-next-todo/src/pages/todos.jsx
@@ -5,48 +5,29 @@ import ListItemText from "@material-ui/core/ListItemText";
 import { Checkbox } from "@material-ui/core";
 import MoreVert from "@bit/mui-org.material-ui-icons.more-vert";
 import { db } from '../../lib/db';
-// import { collection as liteCollection, getDocs } from 'firebase/firestore/lite';
-import { doc, onSnapshot, collection, getDocs } from "firebase/firestore";
+import { onSnapshot, collection, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
     const [todos, setTodos] = useState([]);
 
-    const fetchButton = async () => {
-        const querySnapshot = await getDocs(liteCollection(db, "todos"));
-
-        const _todos = [];
-        querySnapshot.forEach((doc) => {
-            _todos.push({
-                ...doc.data()
-            });
-        });
-        setTodos(_todos)
-    };
-
     useEffect(() => {
             const f = async () => {
                 const querySnapshot = await getDocs(collection(db, "todos"));
-                const docIds = [];
-                querySnapshot.forEach((doc) => {
-                    docIds.push({
-                        id: doc.id
+                const q = query(collection(db, "todos"));
+                const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                    const newTodos = [];
+                    querySnapshot.forEach((doc) => {
+                        newTodos.push({
+                            title: doc.data().title,
+                            content: doc.data().content
+                        });
                     });
-                });
-                
-                docIds.forEach((docId) => {
-                    const unsub = onSnapshot(doc(db, "todos", docId.id), (doc) => {
-                        console.log("Current data: ", doc.data());
-                    });
+                    setTodos(newTodos);
                 });
             };
         f();
     }, []);
 
-    // useEffect(() => {
-    //     const unsub = onSnapshot(doc(db, "todos", "AYxKzrduqWkOfbZzI1oG"), (doc) => {
-    //         console.log("Current data: ", doc.data());
-    //     });
-    // },[]);
     const todoListItems = todos.map(todo => {
         return (
             // keyを設定していないので、idなどを作り設定予定
@@ -60,8 +41,7 @@ export default function Home() {
     return (
         <>
             <List component="nav">
-            <button>取得</button>
-            <div>{todoListItems}</div>
+                {todoListItems}
             </List>
         </>
     )
