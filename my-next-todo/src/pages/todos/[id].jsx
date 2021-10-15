b086f1bf301bd1091065005bae431909424981e1
JunHatanaka <nextseason6@gmail.com>
2021-10-16 00:22:26 +0900

    Added a link from 'todos/[id]/' to 'todos/[id]/edit'.

--- my-next-todo/src/pages/todos/[id].jsx
+++ my-next-todo/src/pages/todos/[id].jsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+import { useRouter } from "next/router";
+
 export default function Home() {
+    const router = useRouter();
+    const todoId = router.query.id;
+
     return (
+        // ドキュメントIDをurlに入れるので、userが一致していないとリダイレクトするようにする！
         <div>
             ・TODO削除<br />
             ・コメント投稿機能
+            <Link href={`/todos/${todoId}/edit`}>
+                todo詳細へ
+            </Link>
         </div>
     )
 }
