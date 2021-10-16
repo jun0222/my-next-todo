aa1ab33f09551ff5653a230545bce1f1352436e4
JunHatanaka <nextseason6@gmail.com>
2021-10-17 02:34:51 +0900

    Added todo editing function.

--- my-next-todo/src/pages/todos/[id]/edit.jsx
+++ my-next-todo/src/pages/todos/[id]/edit.jsx
@@ -1,8 +1,94 @@
+import { db } from '../../../../lib/db';
+import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import { Grid } from '@material-ui/core'
+import { Button } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+
 export default function Home() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [id, setId] = useState('');
+    const router = useRouter();
+
+    // 初回レンダリング時以外（router.asPath !== router.routeがfalse）に、idをステートへセットする。
+    useEffect(() => {
+        if (router.asPath !== router.route) {
+            setId(router.query.id);
+        }
+    }, [router]);
+
+
+    // router.query.idから取得したidを元に、タイトルと本文の初期値を設定。
+    const getDefaultValue = async () =>{
+        if (router.asPath !== router.route) {
+            const docRef = doc(db, "todos", id);
+            const docSnap = await getDoc(docRef);
+            
+            if (docSnap.exists()) {
+                setTitle(docSnap.data().title);
+                setContent(docSnap.data().content);
+            }
+        }
+    }
+    useEffect(() => {
+        getDefaultValue()
+    },[id]);
+
+    // ボタンクリック時にfirestoreのデータを更新する。
+    const updateTodosTask = async () => {
+        if (title === ""){
+            window.alert('タイトルが未入力です')
+            return
+        };
+        if (content === "" ){
+            window.alert('内容が未入力です')
+            return
+        };
+
+        const washingtonRef = doc(db, "todos", id);
+    
+        await updateDoc(washingtonRef, {
+            title: title,
+            content: content
+        });
+
+        window.alert(`以下の内容で登録しました。\n\nタイトル：${title}\n内容：${content}`);
+    }
+
+    // updateTodosTaskで使うためにinputへ入力した値を取得。
+    const getInputTextForTitle = (event) => setTitle(event.target.value);
+    const getInputTextForContent = (event) => setContent(event.target.value);
+
     return (
-        // ドキュメントIDをurlに入れるので、userが一致していないとリダイレクトするようにする！
+        // todo:ドキュメントIDをurlに入れるので、userが一致していないとリダイレクトするようにする！
         <div>
-            ・TODO編集
+            <Grid container>
+                <Grid sm={2}/>
+                <Grid lg={8} sm={8} spacing={10}>
+                <TextField
+                    placeholder="タイトル"
+                    margin="normal"
+                    value={title}
+                    onChange={getInputTextForTitle}
+                />
+                <TextField
+                    placeholder="内容"
+                    margin="normal"
+                    fullWidth
+                    value={content}
+                    onChange={getInputTextForContent}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    margin="normal"
+                    onClick={updateTodosTask}
+                >更新</Button>
+                </Grid>
+            </Grid>
         </div>
     )
 }
