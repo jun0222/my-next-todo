9b4fba9756f49cd3b2df7e64d74b1b1453e09730
JunHatanaka <nextseason6@gmail.com>
2021-10-24 03:19:26 +0900

    Add: Added mypage content.

--- my-next-todo/src/pages/mypage.jsx
+++ my-next-todo/src/pages/mypage.jsx
@@ -1,7 +1,41 @@
+import { app } from '../../lib/firebase';
+import 'firebase/auth';
+import {useState, useEffect} from 'react';
+import {getAuth, signOut, onAuthStateChanged} from 'firebase/auth';
+import { useRouter } from "next/router";
+
 export default function Home() {
+    const auth = getAuth(app);
+    const router = useRouter();
+    const [userEmail, setUserEmail] = useState('');
+
+    const handleSignOut = async (event) => {
+        event.preventDefault();
+        try {
+            const result = await signOut(auth);
+        }catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUserEmail(user.email);
+            } else {
+                router.push('/todos');
+            }
+        })
+    }, []);
+
     return (
-        <div>
-            ・ユーザー情報編集
-        </div>
+        <>
+            <div>
+                <p>メールアドレス：{userEmail}</p>
+                <form onSubmit={handleSignOut} >
+                    <input type="submit" value="sign out" />
+                </form>
+            </div>
+        </>
     )
 }
