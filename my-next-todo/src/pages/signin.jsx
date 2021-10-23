9703185e9622e27bd17a1325f3e15dd0122dd1ad
JunHatanaka <nextseason6@gmail.com>
2021-10-24 01:46:16 +0900

    Add: Added signin function.

--- my-next-todo/src/pages/signin.jsx
+++ my-next-todo/src/pages/signin.jsx
@@ -1,8 +1,69 @@
+import { app } from '../../lib//firebase';
+import 'firebase/auth';
+import {useState, useEffect} from 'react';
+import {getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
+
 export default function Home() {
+    const [isSignedIn, setIsSignedIn] = useState();
+    const auth = getAuth(app);
+
+    const handleSignIn = async (event) => {
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+        try {
+            await signInWithEmailAndPassword(auth, email.value, password.value)
+            console.log(email.value, password.value)
+            alert("Success sign in!!")
+        }catch (error) {
+            console.log(error.message);
+            alert("sign in error!!")
+        }
+    }
+    const handleSignOut = async (event) => {
+        event.preventDefault();
+        try {
+            const result = await signOut(auth);
+            console.log(result);
+        }catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsSignedIn(true);
+                console.log(user);
+            } else {
+                setIsSignedIn(false);
+                console.log('User is signed out');
+            }
+        })
+    }, []);
     return (
+        <>
         <div>
-            ・IDパスワードログイン<br />
-            ・Googleログイン
+            {isSignedIn ? (
+                <form onSubmit={handleSignOut} >
+                    <input type="submit" value="sign out" />
+                </form>
+            ):(
+                <form style={{marginTop: '150px'}} onSubmit={handleSignIn}>
+                    <div>
+                        <label>Enter your email:</label>
+                        <input name="email" type="email"/>
+                    </div>
+                    <div>
+                        <label>Enter your passoword:</label>
+                        <input name="password" type="password"/>
+                    </div>
+                    <div>
+                        <label></label>
+                        <input type="submit" value="Sign in"/>
+                    </div>
+                </form>
+            )}
         </div>
+        </>
     )
 }
